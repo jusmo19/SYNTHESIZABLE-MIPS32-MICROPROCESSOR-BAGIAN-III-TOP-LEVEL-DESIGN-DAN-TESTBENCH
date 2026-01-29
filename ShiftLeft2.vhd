9a8b220c4be5da83bab74caf2add6c93d8c46e0a
Jusen Hapolan Samosir <jusmosir.19@gmail.com>
2026-01-29 09:38:54 +0700

    ShiftLeft2.vhd

---
+++ ShiftLeft2.vhd
@@ -0,0 +1,22 @@
+-- Author: Jusen Samosir
+-- Project: MIPS32 single cycle
+-- Module: ShiftLeft2
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ShiftLeft2 is
+  port( -- input
+        input  : in std_logic_vector(31 downto 0);
+
+        -- output
+        output : out std_logic_vector(31 downto 0) );
+end ShiftLeft2;
+
+architecture Behavioral of ShiftLeft2 is
+begin
+
+  output <= std_logic_vector(unsigned(input) sll 2);
+
+end Behavioral;
